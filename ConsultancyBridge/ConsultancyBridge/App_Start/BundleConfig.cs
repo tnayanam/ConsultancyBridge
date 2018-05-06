3c7034645ccc8a92bfbfe1256881752b85ea5369
tnayanam <nayatanuj@gmail.com>
2018-05-05 23:45:12 -0400

    Custom CSS added

--- ConsultancyBridge/ConsultancyBridge/App_Start/BundleConfig.cs
+++ ConsultancyBridge/ConsultancyBridge/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ConsultancyBridge
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
